6daea968e5fe5ee9d80e06291a34f133e43e399c
prabhat-cn <prabhat@capitalnumbers.com>
2023-10-20 13:04:05 +0530

    done

--- lib/screens/YoutubePlayerIframe.dart
+++ lib/screens/YoutubePlayerIframe.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
+
 class YoutubePlayerIframe extends StatefulWidget {
   const YoutubePlayerIframe({super.key});
 
@@ -8,17 +10,60 @@ class YoutubePlayerIframe extends StatefulWidget {
 }
 
 class _YoutubePlayerIframeState extends State<YoutubePlayerIframe> {
+  late YoutubePlayerController _playerController;
+
+  var autoPlay = true;
+
+  @override
+  void initState() {
+    _playerController = YoutubePlayerController(
+      params: const YoutubePlayerParams(
+        showControls: true,
+        mute: false,
+        showFullscreenButton: true,
+        strictRelatedVideos: true,
+        loop: false,
+        enableJavaScript: true,
+        color: 'red',
+      ),
+    );
+    _playerController.loadVideoById(videoId: '-5bIO0GukLI');
+    _playerController.stopVideo();
+    // _playerController.setSize(
+    //   1000,
+    //   1000,
+    // );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _playerController.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Align(
+        const Align(
           child: Text(
             'Youtube Player Using Iframe',
             style: TextStyle(
               color: Colors.black,
               fontSize: 18,
+            ),
+          ),
+        ),
+        Container(
+          child: YoutubePlayerControllerProvider(
+            controller: _playerController,
+            child: YoutubePlayer(
+              aspectRatio: 16 / 9,
+              controller: _playerController,
+              enableFullScreenOnVerticalDrag: true,
             ),
           ),
         ),
